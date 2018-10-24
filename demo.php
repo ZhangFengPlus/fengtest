6a53ccc4da8fe622e3521277462551ab5272ecaa
zhangfeng <646895437@qq.com>
2018-10-24 14:42:00 +0800

    我提交的第一个版本

---
+++ demo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangfeng
+ * Date: 2018/10/24
+ * Time: 14:24
+ */
+
+require_once './vendor/autoload.php';
+
+use Fengtest\Fengfengtest\Tools;
+
+Tools::Hello();
---
+++ src/Fengtest/Fengfengtest/Tools.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangfeng
+ * Date: 2018/10/24
+ * Time: 14:38
+ */
+namespace Fengtest\Fengfengtest;
+
+class Tools{
+
+    public function __construct()
+    {
+        
+    }
+
+    public static function Hello()
+    {
+        echo "hello world";
+    }
+    
+    
+}
